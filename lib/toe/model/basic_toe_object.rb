210f9a81053bf9282204011ed7f7f6d93deeb9dc
Peter Menke <pmenke@googlemail.com>
2011-11-16 20:16:19 +0100

    created a first subset of the model classes
    Signed-off-by: Peter Menke <pmenke@googlemail.com>

---
+++ lib/toe/model/basic_toe_object.rb
@@ -0,0 +1,11 @@
+module ToE
+  
+  module Model
+
+    # This is a superclass for all objects and entities that need
+    # to be (de)serialized between XML (or other formats) and ToE.
+    class BasicToEObject
+      
+    end
+  end
+end
