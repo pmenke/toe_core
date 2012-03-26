269c8c21a3f090815a6f081c8094ddedb3c1d0a5
Peter Menke <pmenke@googlemail.com>
2012-03-26 17:29:38 +0200

    completed read methods for all possible data types, along with tests.

--- lib/toe/model/data.rb
+++ lib/toe/model/data.rb
@@ -1,33 +1,78 @@
 class ToE::Model::Data
   
+  TRUTH_VALS = %w(true TRUE 1 yes)
+  
   # @return [String|Integer|Double|Boolean|Array|Hash] Ruby representation of this data element
+  # @todo (hi) implement import of the following fields: Integer, Double, Boolean, List
   def self.read(element)
     case element.name
     when "String"
       return ::ToE::Model::Data.read_string(element)
+    when "Int"
+      return ::ToE::Model::Data.read_int(element)
+    when "Float"
+      return ::ToE::Model::Data.read_float(element)
+    when "Bool"
+      return ::ToE::Model::Data.read_bool(element)
+    when "List"
+      return ::ToE::Model::Data.read_list(element)
     when "Map"
-      return ::ToE::Model::Data.read_map(element)
+      return ::ToE::Model::Data.read_map(element)      
     end
   end
   
+  # @todo (hi) method read_int
+  # @todo (hi) read_double
+  # @todo (hi) read_bool
+  # @todo (hi) read_list
   
+  # attempts to read a string from an element
+  # @param   element (XML::Node)  the element to read
+  # @return             (String)  the string value from the element
   def self.read_string(element)
     return element.content
   end
   
+  # attempts to read an integer value from an element
+  # @param   element (XML::Node)  the element to read
+  # @return            (Integer)  the integer value from the element
+  def self.read_int(element)
+    return element.content.to_i
+  end
+  
+  # attempts to read a float value from an element
+  # @param   element (XML::Node)  the element to read
+  # @return              (Float)  the float value from the element
+  def self.read_float(element)
+    return element.content.to_f
+  end  
+  
+  # attempts to read a boolean value from an element
+  # @param   element (XML::Node)  the element to read
+  # @return               (Bool)  the bool value from the element
+  def self.read_bool(element)
+    return (TRUTH_VALS.include?(element.content) ? true : false)
+  end
+  
+  
+  def self.read_list(element)
+    result = Array.new
+    element.each_element do |e|
+      result << ::ToE::Model::Data.read(e)
+    end
+    return result
+  end
+  
   def self.read_map(element)
     x = Hash.new
-    
-    element.children.each do |c|
-      if c.element?
-        key = c.attributes["key"]
-        val = ::ToE::Model::Data.read(c)
-        unless val==nil
-          x[key] = val
-        end
+    element.each_element do |e|
+      key = e.attributes["key"]
+      val = ::ToE::Model::Data.read(e)
+      unless (key==nil || val==nil)
+        x[key] = val
       end
-      return x
     end
+    return x
   end
   
 end
--- test/test_toe_core.rb
+++ test/test_toe_core.rb
@@ -4,4 +4,141 @@ class TestToeCore < Test::Unit::TestCase
   #should "probably rename this file and start testing for real" do
   #  flunk "hey buddy, you should probably rename this file and start testing for real"
   #end
+  
+  def get_data_for_snippet(snippet)
+    parser = ::LibXML::XML::Parser.string(snippet)
+    doc = parser.parse
+    el_data = doc.child
+    puts el_data.name
+    contents = el_data.find_first("./child::*")
+    puts contents.name
+    data = ::ToE::Model::Data.read(contents)
+    return data
+  end
+  
+  def test_3001_string
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <String>HelloWorld</String>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, data.is_a?(String)
+    assert_equal "HelloWorld", data
+  end
+  
+  def test_3011_int
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Int>42</Int>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, data.is_a?(Integer)
+    assert_equal 42, data
+  end
+
+  def test_3021_float
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Float>1234.5678</Float>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, data.is_a?(Float)
+    assert_in_delta 0.001, 1234.5678, data
+  end
+
+  def test_3031_bool
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Bool>true</Bool>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, (data.is_a?(TrueClass)||data.is_a?(FalseClass))
+    assert_equal true, data   
+  end
+
+  def test_3032_bool
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Bool>TRUE</Bool>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, (data.is_a?(TrueClass)||data.is_a?(FalseClass))
+    assert_equal true, data   
+  end
+  
+  def test_3033_bool
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Bool>yes</Bool>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, (data.is_a?(TrueClass)||data.is_a?(FalseClass))
+    assert_equal true, data   
+  end
+  
+  def test_3034_bool
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Bool>1</Bool>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, (data.is_a?(TrueClass)||data.is_a?(FalseClass))
+    assert_equal true, data   
+  end
+  
+  def test_3035_bool
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Bool>0</Bool>
+      </Data>
+    ROSEMARY
+    puts data
+    # assert data
+    assert_equal true, (data.is_a?(TrueClass)||data.is_a?(FalseClass))
+    assert_equal false, data   
+  end
+  
+  def test_4001_list_int
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <List>
+          <Int>1</Int>
+          <Int>2</Int>
+          <Int>3</Int>
+        </List>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal Array, data.class
+    assert_equal 3, data.size
+  end
+  
+    def test_4001_map_int
+    data = get_data_for_snippet(<<-ROSEMARY)
+      <Data>
+        <Map>
+          <Int key="red">1</Int>
+          <Int key="blue">2</Int>
+          <Int key="green">3</Int>
+        </Map>
+      </Data>
+    ROSEMARY
+    assert data
+    assert_equal true, data.is_a?(Hash)
+    # assert_equal 3, data.size
+    assert data.has_key?("red")
+    assert data.has_key?("blue")
+    assert data.has_key?("green")
+    assert_equal 1, data["red"]
+    assert_equal 2, data["blue"]
+    assert_equal 3, data["green"]
+  end
+  
 end
